45f3085a09603cb7ea514d39b65e7c4fde08b2fe
Swapnil Vaidya <vaidyaswapneel@hotmail.com>
2015-11-25 04:36:16 -0800

    Completing current version 1.1.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,9 +6,9 @@ default.basic256.makefile = '/usr/lib64/qt5/bin/qmake'
 
 default.basic256.basicmedia = 1000.0
 
-
+#version for Basic256 application
 default.basic256.remote_version = 'basic256_1.1.2.6.orig.tar.gz'
-
 default.basic256.local_version = 'basic256-1.1.2.6'
 
+#packages to be installed
 default.basic256.package = ['espeak-devel', 'espeak', 'python-espeak', 'openblas-threads', 'mingw64-winpthreads.noarch', 'mingw64-winpthreads-static.noarch']
